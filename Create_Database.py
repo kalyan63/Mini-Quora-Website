0eeda89a2cb817a0dd8b1e9787b0e5c11fa74162
kalyan63 <skalyanreddy123@gmail.com>
2021-11-14 02:23:30 +0530

    Database

--- Create_Database.py
+++ Create_Database.py
@@ -7,7 +7,7 @@ cur=conn.cursor()
 #So all below mentioned data types would be converted to standard data types according to the rules mentioned in the 
 # official website. 
 
-table1='''Create Table User(
+table1='''Create Table IF NOT EXISTS User(
     user_id     INTEGER PRIMARY KEY autoincrement,
     User_name   varchar(20),
     Password    varchar(100),
@@ -15,19 +15,19 @@ table1='''Create Table User(
     Email       varchar(40),
     LastLogin   Text);'''
 
-table2='''Create Table Follow(
+table2='''Create Table IF NOT EXISTS Follow(
     user_id     int,
     follow_id   int,
     Date        Text);'''
 
-table3='''Create Table Questions(
+table3='''Create Table IF NOT EXISTS Questions(
     Q_id        INTEGER PRIMARY KEY autoincrement,
     user_id     int not null,
     Q_Text      varchar(500),
     Ananymous   int default 0,
     Date        Text);'''
 
-table4='''Create Table Answer(
+table4='''Create Table IF NOT EXISTS Answer(
     A_id        INTEGER PRIMARY KEY autoincrement,
     Q_id        int not null,
     user_id     int not null,
@@ -36,17 +36,17 @@ table4='''Create Table Answer(
     Downvote_count  int default 0,
     Date        Text);'''
 
-table5='''Create Table Media(
+table5='''Create Table IF NOT EXISTS Media(
     M_id        INTEGER PRIMARY KEY autoincrement,
     A_id        int,
     File_loc    varchar(40));'''
 
-table6='''Create Table Votes(
+table6='''Create Table IF NOT EXISTS Votes(
     V_id        INTEGER PRIMARY KEY autoincrement,
     A_id        int not null,
     user_id     int not null);'''
 
-table7='''Create Table Comments(
+table7='''Create Table IF NOT EXISTS Comments(
     C_id        INTEGER PRIMARY KEY autoincrement,
     A_id        int not null,
     user_id     int not null,
